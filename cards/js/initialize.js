1548c960f0f6d4073d6871804ead394eb6ad5aff
Maitrey Jukar <maitrey.jukar@zeuslearning.com>
2017-04-22 15:30:54 +0530

    Add initialize js

---
+++ cards/js/initialize.js
@@ -0,0 +1,36 @@
+var deck, hand;
+
+window.onload = init;
+
+function init() {
+
+    deck = new Stack();
+    hand = new Stack();
+
+    deck.makeDeck(1);
+}
+
+function shuffle() {
+
+    if (deck == null) return;
+
+    deck.shuffle(1);
+}
+
+function deal() {
+
+    var i;
+
+    if (deck == null) {
+        return;
+    }
+
+    if (deck.cardCount() < 13) {
+        alert("Not enough cards.");
+    } else {
+        for (i = 0; i < 13; i++) {
+            hand.addCard(deck.deal());
+        }
+    }
+
+}
