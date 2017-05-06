704be6d2e86ae39a9612233e413aa8d9b65e7a75
Maitrey Jukar <maitrey.jukar@zeuslearning.com>
2017-05-07 02:19:18 +0530

    Add edit icon

--- trello/js/views/task.js
+++ trello/js/views/task.js
@@ -7,6 +7,7 @@ Tasker.Views.Task = Backbone.View.extend({
     },
     "createTask": function() {
         this.$el.append("<div class='task-content'>" + this.model.get("content") + "</div>");
+        this.$el.append("<div class='edit-task-content'><div class='edit-icon'></div></div>");
     }
 }, {
 
