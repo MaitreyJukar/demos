970dbeb7746c46ed49af213ca6717d8ff0ce69f2
Maitrey Jukar <maitrey.jukar@zeuslearning.com>
2017-05-07 00:53:22 +0530

    Fix card UI

--- trello/js/views/card.js
+++ trello/js/views/card.js
@@ -2,9 +2,14 @@ Tasker.Views.Card = Backbone.View.extend({
     "initialize": function() {
         this.render();
     },
+    "events": {
+    	"click .add-task": this.addTask
+    },
     "render": function() {
     	this.$el.append("<div class='card-header'><div class='card-title'></div><div class='edit-title'></div></div>")
     	this.$el.append("<div class='tasks-container'></div>");
+    	this.$el.append("<div class='add-task-control'><textarea class='task-content-control'></textarea><div class='add-control'>ADD</div></div>");
+    	this.$el.append("<div class='add-task'>Add Task...</div>");
     	this.$(".card-title").html(this.model.get("title"))
         this.createTasks();
     },
