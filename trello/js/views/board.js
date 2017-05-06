56f7c0fc254a92436d874b4a7b1f0a864440f997
amitkumar.singh <amitkumar.singh@zeuslearning.com>
2017-05-06 23:01:45 +0530

    adds view functionality for cards

--- trello/js/views/board.js
+++ trello/js/views/board.js
@@ -1,12 +1,33 @@
 Tasker.Views.Board = Backbone.View.extend({
     "initialize": function() {
         this.render();
+        this.cardCollectionView = [];
     },
     "render": function() {
         this.createCards();
+        this.makeCardsSortable();
     },
     "createCards": function() {
+        _.each(this.model.get("cardCollection"), function(cardModel, index) {
+            this.cardCollectionView.push(this.createCard(cardModel, null, index));
+        });
+    },
+    "createCard": function(model, title, order) {
+        var cardModel = model || new Tasker.Models.Card({
+                "title": title,
+                "order": order
+            }),
+            cardEl = this.$el.append("<div id='card" + order + "' class='card'></div>");
 
+        return new Tasker.Views.Card({
+            "model": cardModel,
+            "el": cardEl
+        });
+    },
+    "makeCardsSortable": function() {
+        this.$el.sortable({
+
+        });
     }
 }, {
 
